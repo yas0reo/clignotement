adaaadb471b21998447d2264157b4453a19207a1
yasmine <yass.onekeo@gmail.com>
2026-09-09 14:52:02 -0400

    Ajout de la gestion des boutons et de la bibliothèque Bounce2, configuration des recommandations d'extensions pour VSCode

--- clignotement.ino
+++ clignotement.ino
@@ -1,12 +1,38 @@
-// Le code minimal
+#include <Arduino.h>
+#include <Bounce2.h>
 
-#include <Arduino.h> 
+#define BROCHE_DEL 3
+#define BROCHE_BOUTON 2
 
-void setup() {
-  
+Bounce2::Button bouton = Bounce2::Button();
+
+bool etatDel = LOW;
+
+void setup()
+{
+    // Configuration de la DEL
+    pinMode(BROCHE_DEL, OUTPUT);
+    digitalWrite(BROCHE_DEL, etatDel);
+
+    // Configuration du bouton
+    bouton.attach(BROCHE_BOUTON, INPUT_PULLUP);
+    bouton.setPressedState(LOW);
 }
 
-void loop() {
- 
-}
+void loop()
+{
+    // Mise à jour du bouton
+    bouton.update();
 
+    // Une nouvelle pression a-t-elle été détectée ?
+    if (bouton.pressed())
+    {
+        if ( etatDel == 0 ) {
+            etatDel = 1;
+        } else {
+            etatDel = 0;    
+        }
+
+        digitalWrite(BROCHE_DEL, etatDel);
+    }
+}
